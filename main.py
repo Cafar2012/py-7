3c8d6c57b176a677ddf9a102eda0052ffc968d36
Cafar2012 <sachacefer93@gmail.com>
2024-04-27 17:09:17 +0400

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-print("Hello")
+import turtle
+import keyboard
 
-print("New feature")
+ninja = turtle.Turtle()
+
+while True:
+    if keyboard.is_pressed("w"):
+        ninja.forward(10)
+
+    if keyboard.is_pressed("esc"):
+        break
+    
+    if keyboard.is_pressed("s"):
+        ninja.back(10)
+
+    if keyboard.is_pressed("d"):
+        ninja.left(5)
+    
+    
+
+
+
+        
